2d7764b933ea75bed08f99d0de1fabdf867dd8a1
BlumoMariusz <sebastiank24@wp.pl>
2024-04-12 11:27:11 +0200

    change data

--- api/config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // mongoURI: "mongodb://seba:TopDigi12@ds115434.mlab.com:15434/footballmanager",
-  mongoURI: "mongodb+srv://Janeczek:NtGjneYVDbie5g8@cluster0-lryvy.mongodb.net/test_footballmanager?retryWrites=true&w=majority",
-  backupMongoURI: "mongodb+srv://Janeczek:NtGjneYVDbie5g8@cluster0-lryvy.mongodb.net/footballmanager?retryWrites=true&w=majority",
-  // mongoURI: "mongodb+srv://Janeczek:NtGjneYVDbie5g8@cluster0-lryvy.mongodb.net/gallery?retryWrites=true&w=majority",
-  secretOrKey: "secret"
-};
